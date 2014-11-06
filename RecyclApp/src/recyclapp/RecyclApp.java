2694218ea9bcc010adda4d90ff9647c91806b641
SakuraVale <valecotee@gmail.com>
2014-11-06 11:21:19 -0500

    test

--- RecyclApp/src/recyclapp/RecyclApp.java
+++ RecyclApp/src/recyclapp/RecyclApp.java
@@ -15,7 +15,7 @@ public class RecyclApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TESTVALE
     }
     
 }
