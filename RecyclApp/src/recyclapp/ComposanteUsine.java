f4dccfae8538a67f5dd8f03a2d3cabf99bc34902
CactusCode <pascalturcot@hotmail.fr>
2014-11-09 10:11:15 -0500

    ComposanteUsine V1

---
+++ RecyclApp/src/recyclapp/ComposanteUsine.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recyclapp;
+
+/**
+ *
+ * @author Pascal
+ */
+public class ComposanteUsine
+{   
+    private float positionX;
+    private float positionY;
+    private float maximalCapacity;
+    private String name;
+    private String description;
+
+    public ComposanteUsine()
+    {
+        
+    }
+    
+    //Name accessors
+    public void setName(String _value)
+    {
+        
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    //Description accessors
+    public void setDescription(String _value)
+    {
+        
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    //positionX accessors
+    public void setPositionX(float _value)
+    {
+        positionX = _value;
+    }
+    public float getPositionX()
+    {
+        return positionX;
+    }
+    
+    //positionY accessors
+    public void setPositionY(float _value)
+    {
+        positionY = _value;
+    }
+    public float getPositionY()
+    {
+        return positionY;
+    }
+    
+    //maximalCapacity acccessors
+    public void setMaximalCapacity(float _value)
+    {
+        maximalCapacity = _value;
+    }
+    public float getMaximalCapacity()
+    {
+        return maximalCapacity;
+    }
+}
