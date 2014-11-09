121c1d55c910771dfb61ad8e44b9fe63c5893139
SakuraVale <valecotee@gmail.com>
2014-11-09 15:36:22 -0500

    Merge origin/master
    Conflicts:
    	RecyclApp/src/recyclapp/RecyclApp.java

---
+++ RecyclApp/src/Model/Junction.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Vale
+ */
+public class Junction extends ComposanteUsine
+{
+    private ArrayList(point2D _position) list;
+    public Junction()
+    {
+        
+    }
+    public void getPosition()
+    {
+    }
+    
+}
