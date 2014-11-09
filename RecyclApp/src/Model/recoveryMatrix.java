078a55e924a0e3f13bca4b5c8c54a10e55f21296
guignol1981 <vincentguillemette1981@gmail.com>
2014-11-09 16:42:59 -0500

    Grosse partie du model

---
+++ RecyclApp/src/Model/recoveryMatrix.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Vincent
+ */
+public class recoveryMatrix {
+    public class MatrixLine
+    {
+        public Material.MaterialType type;
+        public ArrayList<Integer> stationExits;
+        public ArrayList<Float> pourcentage;
+        public MatrixLine(Material.MaterialType _type, )
+        {
+            this.pourcentage
+        }
+ 
+    }   
+    private ArrayList
+}
